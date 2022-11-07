02f5fbf69fc3be4891eec813f26a50320e833340
Brian Ketelsen <bketelsen@gmail.com>
2022-11-07 15:38:51 +0000

    rel me

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -50,6 +50,7 @@ export default function Document() {
           type="application/feed+json"
           href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
         />
+        <link rel="me" href="https://tty0.social/@bketelsen" />
       </Head>
       <body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
         <Main />
