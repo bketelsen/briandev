1106e68a01b52d8fb00ea902a3f8fe4c47ec6ecc
Brian Ketelsen <bketelsen@gmail.com>
2022-11-07 15:51:01 +0000

    rel me

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -55,6 +55,8 @@ export default function Document() {
       <body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
         <Main />
         <NextScript />
+        <div style={{display: 'none'}}>        <a rel="me" href="https://tty0.social/@bketelsen">Mastodon</a>
+</div>
       </body>
     </Html>
   )
