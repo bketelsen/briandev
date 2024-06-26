809f706e53bda9810a15448c1ab7cef9d111a2ef
Aubrey Zulu <aubreydarious@gmail.com>
2024-06-26 22:43:00 +0200

    docs: ✏️ updated article
    None
    
    BREAKING CHANGE: 🧨 None
    
    ✅ Closes: None

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,9 +1,9 @@
-import { NextSeo } from 'next-seo';
+import { NextSeo } from 'next-seo'
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { getAllArticles } from '@/lib/getAllArticles'
 import { formatDate } from '@/lib/formatDate'
-import  siteMeta  from '@/data/siteMeta'
+import siteMeta from '@/data/siteMeta'
 
 function Article({ article }) {
   return (
@@ -36,32 +36,30 @@ function Article({ article }) {
 
 export default function ArticlesIndex({ articles }) {
   const headline = 'I write about things I’m learning and things I’m building.'
-  const intro="All of my long-form thoughts on programming, leadership, infrastructure, and more, collected in chronological order."
+  const intro =
+    'All of my long-form thoughts on programming, leadership, infrastructure, and more, collected in chronological order.'
 
   return (
     <>
-    <NextSeo
-      title="Articles - Aubrey Zulu"
-      description={siteMeta.description}
-      canonical="https://aubreyzulu.com/articles"
-      openGraph={{
-        url: 'https://aubreyzulu.com/articles',
-        images: [
-          {
-            url: `https://og.aubreyzulu.com/api/og?title=Articles&desc=${headline}`,
-            width: 1200,
-            height: 600,
-            alt: 'Og Image Alt',
-            type: 'image/jpeg',
-          }
-        ],
-        siteName: 'aubreyzulu.com',
-      }}
-    />
-      <SimpleLayout
-        title={headline}
-        intro={intro}
-      >
+      <NextSeo
+        title="Articles - Aubrey Zulu"
+        description={siteMeta.description}
+        canonical="https://aubreyzulu.com/articles"
+        openGraph={{
+          url: 'https://aubreyzulu.com/articles',
+          images: [
+            {
+              url: `https://og.aubreyzulu.com/api/og?title=Articles&desc=${headline}`,
+              width: 1200,
+              height: 600,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+          ],
+          siteName: 'aubreyzulu.com',
+        }}
+      />
+      <SimpleLayout title={headline} intro={intro}>
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
