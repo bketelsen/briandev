171c295f8fcc60ad2031e8e73092e589ea3eb29e
Christian Solis <mail@christiansolis.me>
2024-07-05 14:53:19 -0500

    fix
    fix

--- src/pages/speaking.jsx
+++ src/pages/speaking.jsx
@@ -1,87 +1,87 @@
-import { NextSeo } from 'next-seo';
-import { Card } from '@/components/Card'
-import { Section } from '@/components/Section'
-import { SimpleLayout } from '@/components/SimpleLayout'
-import  siteMeta from '@/data/siteMeta'
+// import { NextSeo } from 'next-seo';
+// import { Card } from '@/components/Card'
+// import { Section } from '@/components/Section'
+// import { SimpleLayout } from '@/components/SimpleLayout'
+// import  siteMeta from '@/data/siteMeta'
 
-function SpeakingSection({ children, ...props }) {
-  return (
-    <Section {...props}>
-      <div className="space-y-16">{children}</div>
-    </Section>
-  )
-}
+// function SpeakingSection({ children, ...props }) {
+//   return (
+//     <Section {...props}>
+//       <div className="space-y-16">{children}</div>
+//     </Section>
+//   )
+// }
 
-function Appearance({ title, description, event, cta, href }) {
-  return (
-    <Card as="article">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
-      <Card.Description>{description}</Card.Description>
-      <Card.Cta>{cta}</Card.Cta>
-    </Card>
-  )
-}
+// function Appearance({ title, description, event, cta, href }) {
+//   return (
+//     <Card as="article">
+//       <Card.Title as="h3" href={href}>
+//         {title}
+//       </Card.Title>
+//       <Card.Eyebrow decorate>{event}</Card.Eyebrow>
+//       <Card.Description>{description}</Card.Description>
+//       <Card.Cta>{cta}</Card.Cta>
+//     </Card>
+//   )
+// }
 
-export default function Speaking() {
-  return (
-    <>
-    <NextSeo
-      title="Speaking - Brian Ketelsen"
-      description={siteMeta.description}
-      canonical="https://brian.dev/speaking"
-      openGraph={{
-        url: 'https://brian.dev/speaking',
-        images: [
-          {
-            url: `https://og.brian.dev/api/og?title=Speaking&desc=I’ve spoken at events all around the world and been interviewed for many podcasts.`,
-            width: 1200,
-            height: 600,
-            alt: 'Og Image Alt',
-            type: 'image/jpeg',
-          }
-        ],
-        siteName: 'brian.dev',
-      }}
-    />
-      <SimpleLayout
-        title="I’ve spoken at events all around the world and been interviewed for many podcasts."
-        intro="One of my favorite ways to share my ideas is live on stage, where there’s so much more communication bandwidth than there is in writing, and I love podcast interviews because they give me the opportunity to answer questions instead of just present my opinions."
-      >
-        <div className="space-y-20">
-          <SpeakingSection title="Conferences">
-          <Appearance
-              href="https://www.youtube.com/watch?v=MzTcsI6tn-0"
-              title="Code Like the Go Team"
-              description="Tips, tricks, and best practices for writing Go code that's idiomatic, readable, and maintainable."
-              event="GopherCon Russia 2018"
-              cta="Watch video"
-            />
-            <Appearance
-              href="https://www.youtube.com/watch?v=mxlJqrVSalY"
-              title="Bringing Go to the Enterprise"
-              description="In this talk I'll share my unique experiences as a trainer and author bringing Go to enterprise companies. We'll talk about what works well, what needs improvement, and what we can do as a community to foster Go adoption in more companies. In 2016 I quit my job and set out to train the world on how to use Go and Kubernetes. Over the course of the last 18 months I've learned a lot of things about Go, and I want to share them with you in this talk."
-              event="Golang UK Conference 2017"
-              cta="Watch video"
-            />
+// export default function Speaking() {
+//   return (
+//     <>
+//     <NextSeo
+//       title="Speaking - Brian Ketelsen"
+//       description={siteMeta.description}
+//       canonical="https://brian.dev/speaking"
+//       openGraph={{
+//         url: 'https://brian.dev/speaking',
+//         images: [
+//           {
+//             url: `https://og.brian.dev/api/og?title=Speaking&desc=I’ve spoken at events all around the world and been interviewed for many podcasts.`,
+//             width: 1200,
+//             height: 600,
+//             alt: 'Og Image Alt',
+//             type: 'image/jpeg',
+//           }
+//         ],
+//         siteName: 'brian.dev',
+//       }}
+//     />
+//       <SimpleLayout
+//         title="I’ve spoken at events all around the world and been interviewed for many podcasts."
+//         intro="One of my favorite ways to share my ideas is live on stage, where there’s so much more communication bandwidth than there is in writing, and I love podcast interviews because they give me the opportunity to answer questions instead of just present my opinions."
+//       >
+//         <div className="space-y-20">
+//           <SpeakingSection title="Conferences">
+//           <Appearance
+//               href="https://www.youtube.com/watch?v=MzTcsI6tn-0"
+//               title="Code Like the Go Team"
+//               description="Tips, tricks, and best practices for writing Go code that's idiomatic, readable, and maintainable."
+//               event="GopherCon Russia 2018"
+//               cta="Watch video"
+//             />
+//             <Appearance
+//               href="https://www.youtube.com/watch?v=mxlJqrVSalY"
+//               title="Bringing Go to the Enterprise"
+//               description="In this talk I'll share my unique experiences as a trainer and author bringing Go to enterprise companies. We'll talk about what works well, what needs improvement, and what we can do as a community to foster Go adoption in more companies. In 2016 I quit my job and set out to train the world on how to use Go and Kubernetes. Over the course of the last 18 months I've learned a lot of things about Go, and I want to share them with you in this talk."
+//               event="Golang UK Conference 2017"
+//               cta="Watch video"
+//             />
 
-          </SpeakingSection>
-          <SpeakingSection title="Podcasts">
-            <Appearance
-              href="https://popcast-d9f7b6dc.simplecast.com/episodes/episode-76-go-roadhouse-with-microsofts-brian-ketelsen"
-              title="
+//           </SpeakingSection>
+//           <SpeakingSection title="Podcasts">
+//             <Appearance
+//               href="https://popcast-d9f7b6dc.simplecast.com/episodes/episode-76-go-roadhouse-with-microsofts-brian-ketelsen"
+//               title="
 
-              Episode 76 - GO Roadhouse with Microsoft's Brian Ketelsen"
-              description="Go, Guitars, Roadhouse, and so much more with Dan Papandrea."
-              event="The POPCAST, July 7, 2021"
-              cta="Listen to podcast"
-            />
+//               Episode 76 - GO Roadhouse with Microsoft's Brian Ketelsen"
+//               description="Go, Guitars, Roadhouse, and so much more with Dan Papandrea."
+//               event="The POPCAST, July 7, 2021"
+//               cta="Listen to podcast"
+//             />
 
-          </SpeakingSection>
-        </div>
-      </SimpleLayout>
-    </>
-  )
-}
+//           </SpeakingSection>
+//         </div>
+//       </SimpleLayout>
+//     </>
+//   )
+// }
--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -1,74 +1,74 @@
-import { NextSeo } from 'next-seo';
-import  siteMeta from '@/data/siteMeta'
-import { Card } from '@/components/Card'
-import { Section } from '@/components/Section'
-import { SimpleLayout } from '@/components/SimpleLayout'
+// import { NextSeo } from 'next-seo';
+// import  siteMeta from '@/data/siteMeta'
+// import { Card } from '@/components/Card'
+// import { Section } from '@/components/Section'
+// import { SimpleLayout } from '@/components/SimpleLayout'
 
-function ToolsSection({ children, ...props }) {
-  return (
-    <Section {...props}>
-      <ul role="list" className="space-y-16">
-        {children}
-      </ul>
-    </Section>
-  )
-}
+// function ToolsSection({ children, ...props }) {
+//   return (
+//     <Section {...props}>
+//       <ul role="list" className="space-y-16">
+//         {children}
+//       </ul>
+//     </Section>
+//   )
+// }
 
-function Tool({ title, href, children }) {
-  return (
-    <Card as="li">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Description>{children}</Card.Description>
-    </Card>
-  )
-}
+// function Tool({ title, href, children }) {
+//   return (
+//     <Card as="li">
+//       <Card.Title as="h3" href={href}>
+//         {title}
+//       </Card.Title>
+//       <Card.Description>{children}</Card.Description>
+//     </Card>
+//   )
+// }
 
-export default function Uses() {
-  return (
-    <>
-    <NextSeo
-      title="Uses - Christian Solis"
-      description={siteMeta.description}
-      canonical="https://brian.dev/uses"
-      openGraph={{
-        url: 'https://brian.dev/uses',
-        images: [
-          {
-            url: `https://og.brian.dev/api/og?title=Uses&desc=Things I use every day to get my work done.`,
-            width: 1200,
-            height: 600,
-            alt: 'Og Image Alt',
-            type: 'image/jpeg',
-          }
-        ],
-        siteName: 'brian.dev',
-      }}
-    />
-      <SimpleLayout
-        title="What I use"
-        intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
-      >
-        <div className="space-y-20">
-          <ToolsSection title="Workstation">
-            <Tool title="13” MacBook Pro, Intel Core i5, 64GB RAM (2020)">
-              I don&apos;t know how Apple makes such a tiny laptop so fast and responsive, no matter what load I put on it.
-            </Tool>
-            <Tool title="HP G5 Monitor">
-              12th Generation i7 with oodles of memory and 4TB of NVMe drives running Bluefin Linux.
-            </Tool>
-            <Tool title="Moonlander Keyboard">
-              I had terrible RSI problems until I switched to the Moonlander and the Apple Magic Trackpad.
-            </Tool>
-            <Tool title="Apple Magic Trackpad">
-              It even works on Linux. Who knew?
-            </Tool>
+// export default function Uses() {
+//   return (
+//     <>
+//     <NextSeo
+//       title="Uses - Christian Solis"
+//       description={siteMeta.description}
+//       canonical="https://brian.dev/uses"
+//       openGraph={{
+//         url: 'https://brian.dev/uses',
+//         images: [
+//           {
+//             url: `https://og.brian.dev/api/og?title=Uses&desc=Things I use every day to get my work done.`,
+//             width: 1200,
+//             height: 600,
+//             alt: 'Og Image Alt',
+//             type: 'image/jpeg',
+//           }
+//         ],
+//         siteName: 'brian.dev',
+//       }}
+//     />
+//       <SimpleLayout
+//         title="What I use"
+//         intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+//       >
+//         <div className="space-y-20">
+//           <ToolsSection title="Workstation">
+//             <Tool title="13” MacBook Pro, Intel Core i5, 64GB RAM (2020)">
+//               I don&apos;t know how Apple makes such a tiny laptop so fast and responsive, no matter what load I put on it.
+//             </Tool>
+//             <Tool title="HP G5 Monitor">
+//               12th Generation i7 with oodles of memory and 4TB of NVMe drives running Bluefin Linux.
+//             </Tool>
+//             <Tool title="Moonlander Keyboard">
+//               I had terrible RSI problems until I switched to the Moonlander and the Apple Magic Trackpad.
+//             </Tool>
+//             <Tool title="Apple Magic Trackpad">
+//               It even works on Linux. Who knew?
+//             </Tool>
 
-          </ToolsSection>
+//           </ToolsSection>
 
-        </div>
-      </SimpleLayout>
-    </>
-  )
-}
+//         </div>
+//       </SimpleLayout>
+//     </>
+//   )
+// }
