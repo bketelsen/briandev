5726b5e0d202bb87b3142c1c0f62ff1ae08d2436
Christian Solis <mail@christiansolis.me>
2024-07-05 14:21:36 -0500

    minor fix
    Uncommented index file

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,83 +1,83 @@
-// import { NextSeo } from 'next-seo';
-// import { Card } from '@/components/Card'
-// import { SimpleLayout } from '@/components/SimpleLayout'
-// import { getAllArticles } from '@/lib/getAllArticles'
-// import { formatDate } from '@/lib/formatDate'
-// import  siteMeta  from '@/data/siteMeta'
+import { NextSeo } from 'next-seo';
+import { Card } from '@/components/Card'
+import { SimpleLayout } from '@/components/SimpleLayout'
+import { getAllArticles } from '@/lib/getAllArticles'
+import { formatDate } from '@/lib/formatDate'
+import  siteMeta  from '@/data/siteMeta'
 
-// function Article({ article }) {
-//   return (
-//     <article className="md:grid md:grid-cols-4 md:items-baseline">
-//       <Card className="md:col-span-3">
-//         <Card.Title href={`/articles/${article.slug}`}>
-//           {article.title}
-//         </Card.Title>
-//         <Card.Eyebrow
-//           as="time"
-//           dateTime={article.date}
-//           className="md:hidden"
-//           decorate
-//         >
-//           {formatDate(article.date)}
-//         </Card.Eyebrow>
-//         <Card.Description>{article.description}</Card.Description>
-//         <Card.Cta>Read article</Card.Cta>
-//       </Card>
-//       <Card.Eyebrow
-//         as="time"
-//         dateTime={article.date}
-//         className="mt-1 hidden md:block"
-//       >
-//         {formatDate(article.date)}
-//       </Card.Eyebrow>
-//     </article>
-//   )
-// }
+function Article({ article }) {
+  return (
+    <article className="md:grid md:grid-cols-4 md:items-baseline">
+      <Card className="md:col-span-3">
+        <Card.Title href={`/articles/${article.slug}`}>
+          {article.title}
+        </Card.Title>
+        <Card.Eyebrow
+          as="time"
+          dateTime={article.date}
+          className="md:hidden"
+          decorate
+        >
+          {formatDate(article.date)}
+        </Card.Eyebrow>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read article</Card.Cta>
+      </Card>
+      <Card.Eyebrow
+        as="time"
+        dateTime={article.date}
+        className="mt-1 hidden md:block"
+      >
+        {formatDate(article.date)}
+      </Card.Eyebrow>
+    </article>
+  )
+}
 
-// export default function ArticlesIndex({ articles }) {
-//   const headline = 'WORK IN PROGRESS'
-//   const intro="All of my long-form thoughts on programming, leadership, infrastructure, and more, collected in chronological order."
+export default function ArticlesIndex({ articles }) {
+  const headline = 'WORK IN PROGRESS'
+  const intro="All of my long-form thoughts on programming, leadership, infrastructure, and more, collected in chronological order."
 
-//   return (
-//     <>
-//     <NextSeo
-//       title="Articles - Brian Ketelsen"
-//       description={siteMeta.description}
-//       canonical="https://brian.dev/articles"
-//       openGraph={{
-//         url: 'https://brian.dev/articles',
-//         images: [
-//           {
-//             url: `https://og.brian.dev/api/og?title=Articles&desc=${headline}`,
-//             width: 1200,
-//             height: 600,
-//             alt: 'Og Image Alt',
-//             type: 'image/jpeg',
-//           }
-//         ],
-//         siteName: 'brian.dev',
-//       }}
-//     />
-//       <SimpleLayout
-//         title={headline}
-//         intro={intro}
-//       >
-//         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-//           <div className="flex max-w-3xl flex-col space-y-16">
-//             {articles.map((article) => (
-//               <Article key={article.slug} article={article} />
-//             ))}
-//           </div>
-//         </div>
-//       </SimpleLayout>
-//     </>
-//   )
-// }
+  return (
+    <>
+    <NextSeo
+      title="Articles - Brian Ketelsen"
+      description={siteMeta.description}
+      canonical="https://brian.dev/articles"
+      openGraph={{
+        url: 'https://brian.dev/articles',
+        images: [
+          {
+            url: `https://og.brian.dev/api/og?title=Articles&desc=${headline}`,
+            width: 1200,
+            height: 600,
+            alt: 'Og Image Alt',
+            type: 'image/jpeg',
+          }
+        ],
+        siteName: 'brian.dev',
+      }}
+    />
+      <SimpleLayout
+        title={headline}
+        intro={intro}
+      >
+        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+          <div className="flex max-w-3xl flex-col space-y-16">
+            {articles.map((article) => (
+              <Article key={article.slug} article={article} />
+            ))}
+          </div>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
 
-// export async function getStaticProps() {
-//   return {
-//     props: {
-//       articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
-//     },
-//   }
-// }
+export async function getStaticProps() {
+  return {
+    props: {
+      articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
+    },
+  }
+}
