04f5e5a0cad33f0ca36b7cf1a8df463f4c459ac9
Christian Solis <46233361+Tacoholic@users.noreply.github.com>
2024-12-28 12:35:15 -0600

    Fixes and article

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ import {
   LinkedInIcon,
   MastodonIcon
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/Me.jpg'
+
 
 // import image3 from '@/images/photos/1102.jpg'
 // import image2 from '@/images/photos/20150716-070618871-3.jpg'
@@ -192,55 +192,14 @@ function Resume() {
     </div>
   )
 }
-
-// function Photos() {
-//   let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
-
-//   return (
-//     <div className="mt-16 sm:mt-20">
-//       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-//         {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-//           <div
-//             key={image.src}
-//             className={clsx(
-//               'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-//               rotations[imageIndex % rotations.length]
-//             )}
-//           >
-//             <Image
-//               src={image}
-//               alt=""
-//               sizes="(min-width: 640px) 18rem, 11rem"
-//               className="absolute inset-0 h-full w-full object-cover"
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
 export default function Home({ articles }) {
   return (
     <>
-    {/* <NextSeo
+    <NextSeo
       title="Christian Solis"
       description={siteMeta.description}
-      canonical="linkedin.com/in/christian-solis1/"
-      openGraph={{
-        url: 'linkedin.com/in/christian-solis1/',
-        images: [
-          {
-            url: `https://og.myurlgoeshere./api/og?title=${siteMeta.title}&desc=${siteMeta.description}`,
-            width: 1200,
-            height: 600,
-            alt: 'Og Image Alt',
-            type: 'image/jpeg',
-          }
-        ],
-        siteName: 'linkedin.com/in/christian-solis1/',
-      }}
-    /> */}
+      canonical="https://christiansolis.netlify.app/"
+    />
       <Container className="mt-9">
         <div className="max-w-2xl text-lg">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
@@ -253,7 +212,7 @@ export default function Home({ articles }) {
           <p className="mt-6 prose dark:prose-invert">
           Hello, my name is Christian Solis. I am a QA Engineer/SDET with experience in manual and automated testing, and the Agile Methodology (Scrum). I consider it a personal challenge to continually find creative ways to break an app in order to increase reliability, scalability, and efficiency. I have a solid knowledge of the Software Development Cycle, proactively collaborating with Engineers and Product Managers, and have managed projects for multiple apps, each with at least 10,000 users. My innate ability to understand business requirements leads me to come up with elaborate test cases for use-case scenarios.
           </p>
-          
+      
           <div className="mt-6 flex gap-6">
       
             <SocialLink
@@ -274,14 +233,13 @@ export default function Home({ articles }) {
           </div>
         </div>
       </Container>
-      {/* <Photos /> */}
       <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-1">
-          {/* <div className="flex flex-col gap-16">
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+          <div className="flex flex-col gap-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />
             ))}
-          </div> */}
+          </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             <Newsletter />
             <Resume />
